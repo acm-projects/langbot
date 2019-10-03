b334556fe3ddaeab074e38259f3271854d367a3e
SaloniSS <sxs170015@utdallas.edu>
2019-10-02 21:41:21 -0500

    Typo corrected

--- App/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAo0kLo5_hqh0qO48Fc9A8BSNzqGgsL9YY",
-    authDomain: "langbot-253616.firebaseapp.com",
-    databaseURL: "https://langbot-253616.firebaseio.com",
-    projectId: "langbot-253616",
-    storageBucket: "langbot-253616.appspot.com",
-    messagingSenderId: "277544622388",
-    appId: "1:277544622388:web:f74ea13c0986460e212d61"
-  };
