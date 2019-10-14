f09bae4adccdc7f753eb9e324369b57e4f639570
SaloniSS <sxs170015@utdallas.edu>
2019-10-14 10:58:16 -0500

    Handled timer console message and dialog box

--- App/App.js
+++ App/App.js
@@ -9,6 +9,17 @@ import { NativeAppEventEmitter } from 'react-native';
 
 import { dialogflowConfig ,  firebaseConfig  } from './env';
 
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+
 /*
 Initializing firebase
 */
