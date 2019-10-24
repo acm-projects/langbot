85bed07f0785a6dee9d5ebf1e6193e5f98397213
pauln15 <nayanpaul1530@gmail.com>
2019-10-23 21:13:47 -0500

    Added Text/Speech toggle within the chat settings page

--- App/views/Settings.js
+++ App/views/Settings.js
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View, Platform } from "react-native";
+import { StyleSheet, Text, View, Platform, Button } from "react-native";
 import React, { Component } from "react";
+import { SegmentedControls } from "react-native-radio-buttons";
+
+const setSelectedOption = item => console.log(item);
 
 class Settings extends Component {
-	render() {
-		return <Text>Hello</Text>;
-	}
+  state = {
+    selectedOption: "TEXT"
+  };
+  render() {
+    return (
+      <View>
+        {/* <Button title="Text/Speech" /> */}
+        <SegmentedControls
+          optionContainerStyle={{
+            paddingTop: 10,
+            paddingBottom: 10
+          }}
+          containerStyle={{
+            marginTop: 7,
+            marginHorizontal: 5
+          }}
+          options={["TEXT", "SPEECH"]}
+          onSelection={setSelectedOption.bind(this)}
+          selectedOption={this.state.selectedOption}
+        />
+      </View>
+    );
+  }
 }
 
 export default Settings;
